438dc1d739e70c3969ffc5577e9939ff1fde7e30
Yvson Nunes <yvsonnunes7@gmail.com>
2024-04-14 14:38:36 -0300

    add encode_rc decode_rc

--- src/encoding.py
+++ src/encoding.py
@@ -63,6 +63,63 @@ def encode_rd(data: bytes, max_dict_len: int) -> list[int]:
 
     return indices, dictionary, symbs_saw
 
+def encode_rc(data: bytes, max_dict_len: int) -> list[int]:
+    curr_dict_len = 256
+    dictionary = {bytes([i]): i for i in range(curr_dict_len)} # Inicializa o dicionário com os 255 valores possíveis para 8 bits
+
+    previous_phrase = bytes()
+    
+    indices = [] # Saída vazia
+    symbs_saw = [] # Informações sobre a compressão
+    
+    code_len = 0
+    qnt_symb = 0
+    qnt_reiniciar = 1
+    last_comp_medio = 8
+    current_comp_medio = 0
+        
+    for symb_int in tqdm(data, desc="Encoding", bar_format=bar_format):
+        
+        current_symb = bytes([symb_int])
+        current_phrase = previous_phrase + current_symb
+        qnt_symb += 1
+            
+        if current_phrase in dictionary:
+            previous_phrase = current_phrase
+        else:
+            indices.append(dictionary[previous_phrase]) # Adiciona o índice da última frase encontrada à saída
+            symbs_saw.append(qnt_symb)
+
+            # Adiciona a nova frase no dicionário caso o dict não esteja cheio
+            if curr_dict_len < max_dict_len:
+                dictionary[current_phrase] = curr_dict_len 
+                curr_dict_len += 1
+
+            # code len
+            if(dictionary[previous_phrase] > 255):
+                    code_len += dictionary[previous_phrase].bit_length()
+            else:
+                code_len += 8
+
+            # monitorar compressão a cada 100 symb se não estiver comprimindo reinicia o dict tolerância de 10⁻4 de diff entre ultima leitura e leitura atual   
+            if (qnt_reiniciar > 0):
+                if (qnt_symb % 100 == 0):
+                    current_comp_medio = code_len / (qnt_symb - 1)
+                    if ((last_comp_medio - current_comp_medio) < 0.0001):
+                        curr_dict_len = 256
+                        dictionary = {bytes([i]): i for i in range(curr_dict_len)}
+                        qnt_reiniciar -= 1 # evita o dict reiniciar várias vezes
+                        where_rc = len(indices)
+                    last_comp_medio = current_comp_medio
+            
+            previous_phrase = current_symb # Continua do símbolo atual que quebrou a sequência de frases encontradas
+    
+    if previous_phrase:
+        indices.append(dictionary[previous_phrase])
+        symbs_saw.append(qnt_symb)
+
+    return indices, dictionary, symbs_saw, where_rc
+
 
 def decode(indices: list[int], max_dict_len: int) -> bytes:
     curr_dict_len = 256
@@ -111,3 +168,9 @@ def decode_rd(indices: list[int], max_dict_len: int) -> bytes:
         prev_phrase = curr_phrase
 
     return bytes(data), dictionary
+
+def decode_rc(indices: list[int], max_dict_len: int, where_rc: int):
+    data_1, _ = decode(indices[:where_rc], max_dict_len)
+    data_2, _ = decode(indices[where_rc:], max_dict_len)
+
+    return data_1 + data_2
